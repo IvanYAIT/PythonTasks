5f660c176b173c7eff5b2451a70052a98d90fb09
IvanYAIT <YAkushikia20@st.ithub.ru>
2025-03-28 15:10:10 +0300

    Solve Task1

---
+++ Module12_Tasks/Task1.py
@@ -0,0 +1,19 @@
+def Sum(number):
+    result = 0
+    for num in range(1, number+1):
+        result += num
+    
+    return result
+
+while True:
+        try:
+            userNumber = int(input("Введите число: "))
+
+            if(userNumber > 0):
+                break
+
+            print("Число должно быть больше 0")
+        except:
+            print("Это не число")
+
+print(f"Я знаю, что сумма чисел от 1 до {userNumber} равна {Sum(userNumber)}")
