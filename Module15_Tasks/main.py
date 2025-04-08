83c4cc5aa5b438f86af0c0d73d9206de21679ec6
IvanYAIT <YAkushikia20@st.ithub.ru>
2025-04-08 14:35:40 +0300

    Solve Task4

--- Module15_Tasks/main.py
+++ Module15_Tasks/main.py
@@ -1,7 +1,9 @@
 from Task1.Task1 import Task1
 from Task2.Task2 import Task2
 from Task3.Task3 import Task3
+from Task4.Task4 import Task4
 
 #Task1.main()
 #Task2.main()
-Task3.main()
+#Task3.main()
+Task4.main()
