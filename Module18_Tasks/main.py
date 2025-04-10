724b3e240d08789ba7d133dab22f5ffeb4ec03f5
IvanYAIT <YAkushikia20@st.ithub.ru>
2025-04-10 22:20:33 +0300

    Solve Task4 and add test for Task4

--- Module18_Tasks/main.py
+++ Module18_Tasks/main.py
@@ -1,7 +1,10 @@
 from Task1.Task1 import Task1
 from Task2.Task2 import Task2
 from Task3.Task3 import Task3
+from Task4.Task4 import Task4
 
 #Task1.main()
 #Task2.main()
-Task3.main()
+#Task3.main()
+task = Task4()
+task.main()
