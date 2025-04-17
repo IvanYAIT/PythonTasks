3568edc14500d7715e80aa3d141e287fa0a4911b
IvanYAIT <ivanyakushik@gmail.com>
2025-04-17 12:43:11 +0300

    Solve Task5

--- Module19_Tasks/main.py
+++ Module19_Tasks/main.py
@@ -1,7 +1,9 @@
 from Task2.Task2 import Task2
 from Task3.Task3 import Task3
 from Task4.Task4 import Task4
+from Task5.Task5 import Task5
 
 #Task2.main()
 #Task3.main()
-Task4.main()
+#Task4.main()
+Task5.main()
