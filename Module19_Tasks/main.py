de1c0f648d1482b36bb39cd78a6f3b795c20de6b
IvanYAIT <ivanyakushik@gmail.com>
2025-04-17 14:25:39 +0300

    Solve Task8 and add test for Task8

--- Module19_Tasks/main.py
+++ Module19_Tasks/main.py
@@ -4,10 +4,12 @@ from Task4.Task4 import Task4
 from Task5.Task5 import Task5
 from Task6.Task6 import Task6
 from Task7.Task7 import Task7
+from Task8.Task8 import Task8
 
 #Task2.main()
 #Task3.main()
 #Task4.main()
 #Task5.main()
 #Task6.main()
-Task7.main()
+#Task7.main()
+Task8.main()
