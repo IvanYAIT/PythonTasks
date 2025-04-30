e8e762357af76bb225a3cfcafbd3985a8f819068
IvanYAIT <ivanyakushik@gmail.com>
2025-04-30 12:12:22 +0300

    Solve task5 and add test for Task5

--- Module23_Tasks/main.py
+++ Module23_Tasks/main.py
@@ -2,6 +2,7 @@ from Task1.Task1 import Task1
 from Task2.Task2 import Task2
 from Task3.Task3 import Task3
 from Task4.Task4 import Task4
+from Task5.Task5 import Task5
 
 #task1 = Task1()
 #task1.main()
@@ -9,5 +10,6 @@ from Task4.Task4 import Task4
 #task2.main()
 #task3 = Task3()
 #task3.main()
-task4 = Task4()
-task4.main()
+#task4 = Task4()
+#task4.main()
+Task5.main()
