1331d650a93ab4bb8e379335b24ec0d579d991a0
IvanYAIT <ivanyakushik@gmail.com>
2025-05-13 12:22:48 +0300

    Solve Task2

--- Module27_Tasks/main.py
+++ Module27_Tasks/main.py
@@ -1,7 +1,13 @@
 from Task1.task1 import Task1
+from Task2.task2 import Task2
 
 def test_decorator():
     task1 = Task1()
     task1.main()
 
-test_decorator()
+def test_delayed_func():
+    task2 = Task2()
+    task2.main()
+
+#test_decorator()
+test_delayed_func()
