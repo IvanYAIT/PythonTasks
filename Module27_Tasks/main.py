0dda244c2db25a0300f4b0efdf7860d415fbcd4d
IvanYAIT <ivanyakushik@gmail.com>
2025-05-13 12:45:07 +0300

    Solve Task3

--- Module27_Tasks/main.py
+++ Module27_Tasks/main.py
@@ -1,5 +1,6 @@
 from Task1.task1 import Task1
 from Task2.task2 import Task2
+from Task3.task3 import Task3
 
 def test_decorator():
     task1 = Task1()
@@ -9,5 +10,12 @@ def test_delayed_func():
     task2 = Task2()
     task2.main()
 
+def tes_logging_decorator():
+    task3 = Task3()
+    task3.main()
+    task3.main_second()
+    task3.do_smth()
+
 #test_decorator()
-test_delayed_func()
+#test_delayed_func()
+tes_logging_decorator()
