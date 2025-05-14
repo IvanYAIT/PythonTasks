270a55029004a102de6eeba4bc769532bc71fc91
IvanYAIT <ivanyakushik@gmail.com>
2025-05-14 16:45:15 +0300

    Solve Task1

---
+++ Module28_Tasks/main.py
@@ -0,0 +1,8 @@
+from Task1.task1 import Task1
+
+def write_to_file():
+    path = 'C:\\Users\\Якушик И_А\\Documents\\work\\PythonTasks\\Module28_Tasks\\Task1\\file.txt'
+    task1 = Task1()
+    task1.open_file(path)
+
+write_to_file()
