6dc8ba80dc4f3ea616c5291f06926ae8c8745ef3
IvanYAIT <ivanyakushik@gmail.com>
2025-05-15 13:59:04 +0300

    Solve Task3

--- Module29_Tasks/main.py
+++ Module29_Tasks/main.py
@@ -1,5 +1,6 @@
 from Task1.task1 import User, PermissionType
 from Task2.task2 import Task2
+from Task3.task3 import Task3Child
 
 def user_actions():
     user = User()
@@ -16,5 +17,11 @@ def user_actions():
 def check_callback_func():
     Task2.main()
 
+def print_logs():
+    task3 = Task3Child()
+    task3.test_func1()
+    task3.test_func2()
+
 #user_actions()
-check_callback_func()
+#check_callback_func()
+print_logs()
